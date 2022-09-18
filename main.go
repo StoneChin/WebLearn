d170a62134b887ddf1d4e3def2e24b359b5b72f2
Stone <935434967@qq.com>
2022-09-18 12:23:14 +0800

    first push

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusAccepted, "Hello World!")
+	})
+	r.GET("/news", func(c *gin.Context) {
+		c.String(http.StatusAccepted, "This is news page")
+	})
+	r.Run()
+}
